fcacf83f734686908cba3025a2d3ffa5a315589f
Wenyao Chen <wenyaochen427@gmail.com>
2021-01-29 01:27:48 +0800

    update

---
+++ image.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+from tkinter import *
+
+root = Tk()
+root.title("image")
+root.iconbitmap('D:\programming\Tkinter_practice\image\\favicon.ico')
+
+
+root.mainloop()
