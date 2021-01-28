f9c3b9288cfbd55ef7d3322860a200bdf24eeba9
Wenyao Chen <wenyaochen427@gmail.com>
2021-01-29 01:17:37 +0800

    calculutor

--- button.py
+++ button.py
@@ -8,9 +8,10 @@ root = Tk()
 
 e = Entry(root, width=50)
 e.pack()
+e.insert(0, "Enter Your Name:")  #defult value
 
 def myClick():
-    myLabel = Label(root, text=e.get())
+    myLabel = Label(root, text="hello " + e.get())
     myLabel.pack()
 
 myButton = Button(root, text="Enter your name", padx=50, command=myClick, fg="red")
